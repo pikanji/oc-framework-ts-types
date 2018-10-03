b3f592dbe7a9c7ceaaf1237e5ec0bb5fb9563cf5
Kanji Furuhashi <pikanji@gmail.com>
2018-10-03 11:09:15 +0900

    Add header comment

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for OctoberCMS framework.js v1.0.443
+// Project: https://octobercms.com/
+// Definitions by: Kanji Furuhashi <https://github.com/pikanji/>
+// Definitions: https://github.com/pikanji/oc-framework-ts-types
+
 interface JQueryStatic {
     request(handler: string, options: any): any;
 }
